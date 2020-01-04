88115f3bb763adb9e7bc835b528b1af64e908b49
Antonio Herrera <37684203+Azureknight117@users.noreply.github.com>
2020-01-04 00:54:03 -0600

    Made a scriptable object for the sword pieces

---
+++ Ashriel&TheBrokenSword/Assets/Scripts/Sword/SwordManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordManager : MonoBehaviour
+{
+    public GameObject middlePieceHolder;
+    public GameObject tipPieceHolder;
+
+    [Header("SwordPieces")]
+    public SwordPiece mid;
+    public SwordPiece tip;
+
+    [HideInInspector]
+    float totalDamage; //Combined damage of all pieces
+}
---
+++ Ashriel&TheBrokenSword/Assets/Scripts/Sword/SwordPiece.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SwordPiece", menuName = "SwordPiece")]
+public class SwordPiece : ScriptableObject
+{
+    public enum PieceType //Three types of sword pieces, a tip, mid, and full (a mid and a tip combined)
+    {
+        mid,
+        tip,
+        full     
+    }
+
+    public enum RarityType //common is dropped by enemies, rare is occasionally dropped by enemy or found in chests, legend types can only be found in treasure chests or from bosses
+    {
+        common,
+        rare,
+        legend
+    }
+
+    public PieceType type;
+    public RarityType rarity;
+
+    public string pieceName;
+    public string description;
+    public float power;//amount of damage it does, each piece has its own.
+    public int length;//how long the piece is
+    //higher length should mean less damage and vice versa
+    public Sprite sprite;
+
+}
